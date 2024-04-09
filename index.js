a0807280f4c112f9201d81e50c6a79cfafd13993
carlosDevf <carlos@devf.mx>
2024-04-09 00:10:41 -0600

    mi primer nodejs

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const { suma, multi } = require('./operaciones')
 const { vehiculos } = require('./CONSTANS')
-// modulo nativo
 const os = require('os')
-
+// modulo nativo
 
 console.log(suma(1000, 20))
 console.log(multi(200, 2))
